c0da8a138e3f5657d1474358be4ffe0171939f3a
xamxo8928-cmd <xamxo8928@gmail.com>
2026-07-08 15:26:17 +0300

    Update market.py

--- market.py
+++ market.py
@@ -1,24 +1,40 @@
 import yfinance as yf
 
-pairs = {
-    "EURUSD": "EURUSD=X",
-    "GBPUSD": "GBPUSD=X",
-    "USDJPY": "JPY=X",
-    "AUDUSD": "AUDUSD=X",
-    "USDCAD": "CAD=X"
+PAIRS = {
+    "EUR/USD": "EURUSD=X",
+    "GBP/USD": "GBPUSD=X",
+    "USD/JPY": "JPY=X",
+    "AUD/USD": "AUDUSD=X",
+    "USD/CAD": "CAD=X",
+    "USD/CHF": "CHF=X",
+    "NZD/USD": "NZDUSD=X"
 }
 
-def get_prices():
-    prices = {}
+def get_market_data():
+    result = {}
 
-    for name, ticker in pairs.items():
+    for pair_name, ticker in PAIRS.items():
         try:
-            data = yf.download(ticker, period="1d", interval="1m", progress=False)
+            df = yf.download(
+                ticker,
+                period="2d",
+                interval="1m",
+                progress=False,
+                auto_adjust=False
+            )
 
-            if not data.empty:
-               prices[name] = round(data["Close"].values[-1].item(), 5)
+            if df.empty:
+                continue
+
+            df = df.dropna()
+
+            # Если yfinance вернул MultiIndex
+            if hasattr(df.columns, "levels"):
+                df.columns = [c[0] for c in df.columns]
+
+            result[pair_name] = df.tail(250)
 
         except Exception as e:
-            print(f"Ошибка {name}: {e}")
+            print(f"{pair_name}: {e}")
 
-    return prices
+    return result
