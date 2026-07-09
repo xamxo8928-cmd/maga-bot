1c7029157edc2d4e9425383252588a7eae22c45c
xamxo8928-cmd <xamxo8928@gmail.com>
2026-07-09 09:27:37 +0300

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -13,9 +13,9 @@ def rsi(series, period=14):
 
     return 100 - (100 / (1 + rs))
 
-def check_signal(df_m1, df_m5=None):
+def check_signal(df):
 
-    close = df_m1["Close"]
+    close = df["Close"]
 
     ema20 = ema(close, 20)
     ema50 = ema(close, 50)
@@ -23,35 +23,19 @@ def check_signal(df_m1, df_m5=None):
 
     rsi14 = rsi(close, 14)
 
-    last = len(df_m1) - 1
-
-    trend_ok = True
-
-    if df_m5 is not None:
-        close5 = df_m5["Close"]
-        ema20_5 = ema(close5, 20)
-        ema50_5 = ema(close5, 50)
-
-        trend_ok = ema20_5.iloc[-1] > ema50_5.iloc[-1]
+    last = len(df) - 1
 
     if (
         ema20.iloc[last] > ema50.iloc[last] > ema200.iloc[last]
         and close.iloc[last] > ema20.iloc[last]
         and rsi14.iloc[last] > 60
-        and trend_ok
     ):
         return "CALL"
-
-    trend_ok = True
-
-    if df_m5 is not None:
-        trend_ok = ema20_5.iloc[-1] < ema50_5.iloc[-1]
 
     if (
         ema20.iloc[last] < ema50.iloc[last] < ema200.iloc[last]
         and close.iloc[last] < ema20.iloc[last]
         and rsi14.iloc[last] < 40
-        and trend_ok
     ):
         return "PUT"
 
