191645847cbd690495cb6fcc6db75225793d6253
xamxo8928-cmd <xamxo8928@gmail.com>
2026-07-09 21:02:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, request
 import requests
 import os
-from market import get_market_data
+
+from threading import Thread
+
+from cache import market_cache
 from strategy import check_signal
 from telegram_bot import send_signal
-from threading import Thread
 from scheduler import run_scheduler
 from database import init_db
 
 app = Flask(__name__)
+
 init_db()
+
 thread = Thread(target=run_scheduler)
 thread.daemon = True
 thread.start()
@@ -17,59 +21,67 @@ thread.start()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
     payload = {
         "chat_id": CHAT_ID,
         "text": message,
         "parse_mode": "HTML"
     }
+
     requests.post(url, data=payload)
+
 
 @app.route("/")
 def home():
     return "Bot is running!"
 
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
+
     data = request.json
 
     if not data:
         return "no data", 400
 
-    # TradingView отправляет текст
     message = data.get("message", "No signal")
 
     send_telegram(message)
 
     return "ok", 200
 
+
 @app.route("/signal")
 def signal():
 
-    data = get_market_data()
-
     result = {}
 
-    for pair, df in data.items():
+    for pair, df in market_cache.items():
 
-        signal = check_signal(df)
+        sig = check_signal(df)
 
-        result[pair] = signal if signal else "WAIT"
+        result[pair] = sig if sig else "WAIT"
 
     return result
+
+
 @app.route("/prices")
 def prices():
-    data = get_market_data()
 
     result = {}
 
-    for pair, df in data.items():
+    for pair, df in market_cache.items():
+
         result[pair] = {
             "close": round(float(df["Close"].iloc[-1]), 5),
             "candles": len(df)
         }
 
     return result
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
