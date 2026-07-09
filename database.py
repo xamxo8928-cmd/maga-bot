0d023ddbfcaec40c4a168ae34587780c3f5d5d7e
xamxo8928-cmd <xamxo8928@gmail.com>
2026-07-09 14:46:08 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,24 +3,37 @@ import sqlite3
 DB_NAME = "signals.db"
 
 def init_db():
+
     conn = sqlite3.connect(DB_NAME)
 
     cursor = conn.cursor()
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS signals (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            pair TEXT,
-            signal TEXT,
-            price REAL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
+    CREATE TABLE IF NOT EXISTS signals(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        pair TEXT,
+
+        signal TEXT,
+
+        entry_price REAL,
+
+        exit_price REAL,
+
+        result TEXT,
+
+        checked INTEGER DEFAULT 0,
+
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    )
     """)
 
     conn.commit()
     conn.close()
 
-def save_signal(pair, signal, price):
+def save_signal(pair, signal, entry_price):
 
     conn = sqlite3.connect(DB_NAME)
 
@@ -28,11 +41,59 @@ def save_signal(pair, signal, price):
 
     cursor.execute(
         """
-        INSERT INTO signals(pair, signal, price)
-        VALUES (?, ?, ?)
+        INSERT INTO signals(pair, signal, entry_price)
+        VALUES(?,?,?)
         """,
-        (pair, signal, price)
+        (
+            pair,
+            signal,
+            entry_price
+        )
     )
 
     conn.commit()
+    conn.close()
+
+def get_unchecked():
+
+    conn = sqlite3.connect(DB_NAME)
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id,pair,signal,entry_price
+        FROM signals
+        WHERE checked=0
+    """)
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return rows
+
+def update_result(signal_id, exit_price, result):
+
+    conn = sqlite3.connect(DB_NAME)
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE signals
+
+        SET
+            exit_price=?,
+            result=?,
+            checked=1
+
+        WHERE id=?
+    """,
+    (
+        exit_price,
+        result,
+        signal_id
+    ))
+
+    conn.commit()
+
     conn.close()
