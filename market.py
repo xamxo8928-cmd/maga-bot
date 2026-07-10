8b050221a778c58e252a794ce21d98e7bffd256f
xamxo8928-cmd <xamxo8928@gmail.com>
2026-07-10 14:06:17 +0300

    Update market.py

--- market.py
+++ market.py
@@ -3,7 +3,9 @@ import time
 import requests
 import pandas as pd
 
+
 API_KEY = os.getenv("TWELVE_API_KEY")
+
 
 PAIRS = {
     "EUR/USD": "EUR/USD",
@@ -15,18 +17,23 @@ PAIRS = {
     "NZD/USD": "NZD/USD",
 }
 
+
 last_data = {}
 
 
 def get_market_data():
 
+    print("MARKET VERSION WITHOUT VOLUME")
+
     global last_data
 
     result = {}
 
+
     for pair_name, symbol in PAIRS.items():
 
         print(f"Загружаю {pair_name}")
+
 
         try:
 
@@ -42,12 +49,16 @@ def get_market_data():
                 timeout=10
             )
 
+
             data = response.json()
+
 
             if "values" not in data:
                 raise Exception(data)
 
+
             df = pd.DataFrame(data["values"])
+
 
             df = df.rename(columns={
                 "datetime": "Datetime",
@@ -57,25 +68,53 @@ def get_market_data():
                 "close": "Close"
             })
 
-            df["Datetime"] = pd.to_datetime(df["Datetime"])
-            df = df.set_index("Datetime")
+
+            df["Datetime"] = pd.to_datetime(
+                df["Datetime"]
+            )
+
+
+            df = df.set_index(
+                "Datetime"
+            )
+
+
             df = df.sort_index()
 
-            for col in ["Open", "High", "Low", "Close"]:
+
+            for col in [
+                "Open",
+                "High",
+                "Low",
+                "Close"
+            ]:
                 df[col] = df[col].astype(float)
 
+
             result[pair_name] = df
+
             last_data[pair_name] = df
 
-            print(f"{pair_name}: {len(df)} свечей")
+
+            print(
+                f"{pair_name}: {len(df)} свечей"
+            )
+
 
         except Exception as e:
 
-            print(f"{pair_name}: ошибка API ({e}), беру старые данные")
+
+            print(
+                f"{pair_name}: ошибка API ({e}), беру старые данные"
+            )
+
 
             if pair_name in last_data:
+
                 result[pair_name] = last_data[pair_name]
 
+
         time.sleep(1)
+
 
     return result
