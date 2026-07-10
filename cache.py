80dd7f725fce66c8f1f1e9eccb4ed6ae0e68a33a
xamxo8928-cmd <xamxo8928@gmail.com>
2026-07-10 13:53:37 +0300

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,19 +1,29 @@
-import pickle
 import os
+import pickle
 
 
-CACHE_FILE = "market_cache.pkl"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+CACHE_FILE = os.path.join(BASE_DIR, "market_cache.pkl")
 
 
 def save_cache(data):
-    with open(CACHE_FILE, "wb") as f:
-        pickle.dump(data, f)
+    try:
+        with open(CACHE_FILE, "wb") as f:
+            pickle.dump(data, f)
+    except Exception as e:
+        print(f"Ошибка сохранения cache: {e}")
 
 
 def load_cache():
-    if not os.path.exists(CACHE_FILE):
-        return {}
+    try:
+        if not os.path.exists(CACHE_FILE):
+            return {}
 
-    with open(CACHE_FILE, "rb") as f:
-        return pickle.load(f)
+        with open(CACHE_FILE, "rb") as f:
+            return pickle.load(f)
+
+    except Exception as e:
+        print(f"Ошибка загрузки cache: {e}")
+        return {}
 
