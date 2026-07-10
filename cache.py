6cdee6b93b9547d01d45b249fb5cbe3a2056f013
xamxo8928-cmd <xamxo8928@gmail.com>
2026-07-10 03:24:30 +0300

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,2 +1,19 @@
-market_cache = {}
+import pickle
+import os
+
+
+CACHE_FILE = "market_cache.pkl"
+
+
+def save_cache(data):
+    with open(CACHE_FILE, "wb") as f:
+        pickle.dump(data, f)
+
+
+def load_cache():
+    if not os.path.exists(CACHE_FILE):
+        return {}
+
+    with open(CACHE_FILE, "rb") as f:
+        return pickle.load(f)
 
